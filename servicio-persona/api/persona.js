2cd2b51295e3b3cda45f042a935218eb3838d73b
Rodrigo Mendoza Melgar <r.mendozam@ulasalle.edu.pe>
2020-10-18 17:54:44 -0500

    se agrego la funcionalidad de listado

--- servicio-persona/api/persona.js
+++ servicio-persona/api/persona.js
@@ -11,7 +11,66 @@ module.exports.guardar = (event, context, callback) => {
 	
 	const requestBody = JSON.parse(event.body);
 	
+	personaPersistir(personaCrear(requestBody))
+    .then(persona => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: `Se guardo exitosamente la persona ${persona.nombre_apellidos}`,
+          persona: persona
+        })
+      });
+    })
+    .catch(error => {
+      console.log(error);
+	  
+      callback(null, {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: `No se logro guardar a la persona ${persona.nombre_apellidos}`,
+        })
+      })
+    });
+};
+
+const personaPersistir = persona => {
+	if(persona == undefined)
+		return;
+	
+	console.log('Persistiendo persona');
+	
+	const personaDB = {
+		TableName: process.env.TABLA_PERSONA,
+		Item: persona,
+	};
+	return dynamoDb.put(personaDB).promise()
+		.then(resultado => persona);
+};
+
+
+const personaCrear = requestBody => {
+	
 	let persona = {};
+	
+	persona = personaSet(requestBody);
+	
+	persona = personaEsValida(persona);
+	
+	if(persona == undefined)
+		return;
+	
+	const timestamp = new Date().getTime();
+	
+	persona.id = uuid.v1();
+	persona.creado = timestamp;
+	persona.editado = timestamp;
+	
+	return persona;
+}
+
+const personaSet = requestBody =>{
+	let persona = {};
+	
 	persona.nombre_apellidos = requestBody.nombre_apellidos;
 	persona.anio_nacimiento = requestBody.anio_nacimiento;
 	persona.color_ojos = requestBody.color_ojos;
@@ -26,45 +85,10 @@ module.exports.guardar = (event, context, callback) => {
 	persona.naves_estelares = requestBody.naves_estelares;
 	persona.vehiculos = requestBody.vehiculos;
 	
-	personaPersistir(personaCrear(persona))
-    .then(resultado => {
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: `Se guardo exitosamente la persona ${persona.nombre_apellidos}`,
-          persona: resultado
-        })
-      });
-    })
-    .catch(error => {
-      console.log(error);
-	  
-      callback(null, {
-        statusCode: 500,
-        body: JSON.stringify({
-          message: `No se logro guardar a la persona ${persona.nombre_apellidos}`,
-        })
-      })
-    });
-	
-	
-};
+	return persona;
+}
 
-const personaPersistir = persona => {
-	console.log('Persistiendo persona');
-	
-	const personaDB = {
-		TableName: process.env.TABLA_PERSONA,
-		Item: persona,
-	};
-	return dynamoDb.put(personaDB).promise()
-		.then(resultado => persona);
-};
-
-
-const personaCrear = persona => {
-	const timestamp = new Date().getTime();
-	
+const personaEsValida = persona =>{
 	if (typeof persona.nombre_apellidos !== 'string' || 
 		typeof persona.anio_nacimiento !== 'string' || 
 		typeof persona.color_ojos !== 'string' ||
@@ -83,23 +107,33 @@ const personaCrear = persona => {
 		callback(new Error('No se logro guardar la persona, fallo al validar la persona'));
 		return;
 	}
+	return persona;
+}
+
+module.exports.listar = (event, context, callback) => {
+	var parametros = {
+        TableName: process.env.TABLA_PERSONA,
+        ProjectionExpression: "id, nombre_apellidos, anio_nacimiento, color_ojos, genero, color_cabello, altura, masa, color_piel, url_planeta, peliculas, especies, naves_estelares, vehiculos, creado, editado"
+    };
+
+    console.log("Leyendo tabla persona");
 	
-	return {
-		id: uuid.v1(),
-		nombre_apellidos: persona.nombre_apellidos,
-		anio_nacimiento: persona.anio_nacimiento,
-		color_ojos: persona.color_ojos,
-		genero: persona.genero,
-		color_cabello: persona.color_cabello,
-		altura: persona.altura,
-		masa: persona.masa,
-		color_piel: persona.color_piel,
-		url_planeta: persona.url_planeta,
-		peliculas: persona.peliculas,
-		especies: persona.especies,
-		naves_estelares: persona.naves_estelares,
-		vehiculos: persona.vehiculos,
-		creado: timestamp,
-		editado: timestamp,
-	};
+    const onLeerTabla = (error, resultado) => {
+
+        if (error) {
+            console.log('Fallo al leer los datos:', JSON.stringify(error, null, 2));
+            callback(error);
+        } else {
+            console.log("Lectura exitosa");
+			
+            return callback(null, {
+                statusCode: 200,
+                body: JSON.stringify({
+                    personas: resultado.Items
+                })
+            });
+        }
+    };
+
+    dynamoDb.scan(parametros, onLeerTabla);
 }
