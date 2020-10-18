8dcfbe3a752ef9dab2227708383ae7a3cb7936ac
Rodrigo Mendoza Melgar <r.mendozam@ulasalle.edu.pe>
2020-10-18 15:16:33 -0500

    se agrego los parametros del objeto persona

--- servicio-persona/api/persona.js
+++ servicio-persona/api/persona.js
@@ -1,10 +1,34 @@
 'use strict';
 
+const uuid = require('uuid');
+const AWS = require('aws-sdk'); 
+
+AWS.config.setPromisesDependency(require('bluebird'));
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
 module.exports.guardar = (event, context, callback) => {
+	
+	const requestBody = JSON.parse(event.body);
+	
+	let persona = {};
+	persona.nombre_apellidos = requestBody.nombre_apellidos;
+	persona.anio_nacimiento = requestBody.anio_nacimiento;
+	persona.color_ojos = requestBody.color_ojos;
+	persona.genero = requestBody.genero;
+	persona.color_cabello = requestBody.color_cabello;
+	persona.altura = requestBody.altura;
+	persona.masa = requestBody.masa;
+	persona.color_piel = requestBody.color_piel;
+	persona.url_planeta = requestBody.url_planeta;
+	persona.peliculas = requestBody.peliculas;
+	persona.especies = requestBody.especies;
+	persona.naves_estelares = requestBody.naves_estelares;
+	persona.vehiculos = requestBody.vehiculos;
+	
+	
   callback(null, {
         statusCode: 200,
-        body: JSON.stringify({
-          message: `Se agrego la persona`
-        })
+        body: JSON.stringify(persona)
   });
 };
