c94eb443406b4cf88f816d924d8c3e38310fb06a
Rodrigo Mendoza Melgar <r.mendozam@ulasalle.edu.pe>
2020-10-19 00:35:02 -0500

    se finalizo el servicio de personas

--- servicio-persona/api/persona.js
+++ servicio-persona/api/persona.js
@@ -1,76 +1,75 @@
 'use strict';
 
-const uuid = require('uuid');
 const AWS = require('aws-sdk'); 
+
+const swapi = require('swapi-node');
 
 AWS.config.setPromisesDependency(require('bluebird'));
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-module.exports.guardar = (event, context, callback) => {
+module.exports.guardar = async(event, context, callback) => {
 	
 	const requestBody = JSON.parse(event.body);
-	
-	personaPersistir(personaCrear(requestBody))
-    .then(persona => {
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: `Se guardo exitosamente la persona ${persona.nombre_apellidos}`,
-          persona: persona
-        })
-      });
-    })
-    .catch(error => {
-      console.log(error);
-	  
-      callback(null, {
-        statusCode: 500,
-        body: JSON.stringify({
-          message: `No se logro guardar a la persona ${persona.nombre_apellidos}`,
-        })
-      })
-    });
-};
-
-const personaPersistir = persona => {
+	let persona = convertirRequestBodyToPersona(requestBody);
+	persona = validar(persona);
 	if(persona == undefined)
 		return;
-	
-	console.log('Persistiendo persona');
-	
-	const personaDB = {
-		TableName: process.env.TABLA_PERSONA,
-		Item: persona,
-	};
-	return dynamoDb.put(personaDB).promise()
-		.then(resultado => persona);
+	await guardar(persona);
+	return callback(null, getRespuesta(persona));
 };
 
+module.exports.listar = async(event, context, callback) => {
+	return callback(null, getRespuesta(await encontrarTodos()));
+};
 
-const personaCrear = requestBody => {
+module.exports.get = async(event, context, callback) => {
+	const _id = event.pathParameters.id;
 	
-	let persona = {};
+	let persona = await encontrarPorId(_id);
 	
-	persona = personaSet(requestBody);
-	
-	persona = personaEsValida(persona);
-	
-	if(persona == undefined)
-		return;
-	
-	const timestamp = new Date().getTime();
-	
-	persona.id = uuid.v1();
-	persona.creado = timestamp;
-	persona.editado = timestamp;
-	
-	return persona;
+	if(persona === undefined){
+		let people = await encontrarEnSWAPI(_id);
+		people.id = _id;
+		
+		let persona = convertirPeopleToPersona(people);
+		await guardar(persona);
+		return callback(null, getRespuesta(persona));
+		
+	}
+	return callback(null, getRespuesta(persona));
+};
+
+async function encontrarEnSWAPI(id){
+	let people = await swapi.getPerson(id);
+	return people;
 }
 
-const personaSet = requestBody =>{
+function convertirPeopleToPersona(people){
 	let persona = {};
 	
+	persona.id = people.id;
+	persona.nombre_apellidos = people.name;
+	persona.anio_nacimiento = people.birth_year;;
+	persona.color_ojos = people.eye_color;
+	persona.genero = people.gender;
+	persona.color_cabello = people.hair_color;
+	persona.altura = people.height;
+	persona.masa = people.mass;
+	persona.color_piel = people.skin_color;
+	persona.url_planeta = people.homeworld;
+	persona.peliculas = people.films;
+	persona.especies = people.species;
+	persona.naves_estelares = people.starships;
+	persona.vehiculos = people.vehicles;
+	
+	return persona;
+};
+
+function convertirRequestBodyToPersona (requestBody){
+	let persona = {};
+	
+	persona.id = requestBody.id;
 	persona.nombre_apellidos = requestBody.nombre_apellidos;
 	persona.anio_nacimiento = requestBody.anio_nacimiento;
 	persona.color_ojos = requestBody.color_ojos;
@@ -86,10 +85,11 @@ const personaSet = requestBody =>{
 	persona.vehiculos = requestBody.vehiculos;
 	
 	return persona;
-}
+};
 
-const personaEsValida = persona =>{
-	if (typeof persona.nombre_apellidos !== 'string' || 
+function validar(persona){
+	if (typeof persona.id !== 'string' ||
+		typeof persona.nombre_apellidos !== 'string' ||	
 		typeof persona.anio_nacimiento !== 'string' || 
 		typeof persona.color_ojos !== 'string' ||
 		typeof persona.genero !== 'string' ||
@@ -104,36 +104,50 @@ const personaEsValida = persona =>{
 		!Array.isArray(persona.vehiculos)
 	){
 		console.error('Fallo al validar la persona');
-		callback(new Error('No se logro guardar la persona, fallo al validar la persona'));
 		return;
 	}
 	return persona;
-}
+};
 
-module.exports.listar = (event, context, callback) => {
-	var parametros = {
+async function encontrarTodos () {
+	const parametros = {
         TableName: process.env.TABLA_PERSONA,
         ProjectionExpression: "id, nombre_apellidos, anio_nacimiento, color_ojos, genero, color_cabello, altura, masa, color_piel, url_planeta, peliculas, especies, naves_estelares, vehiculos, creado, editado"
     };
 
-    console.log("Leyendo tabla persona");
+    let resultado = await dynamoDb.scan(parametros).promise();
+	return resultado.Items;
+};
+
+async function encontrarPorId (_id) {
+	const parametros = {
+		TableName: process.env.TABLA_PERSONA,
+		Key: {
+			id: _id,
+		}
+	};
+	let resultado = await dynamoDb.get(parametros).promise();
+	return resultado.Item;
+};
+
+async function guardar(persona){
+	const timestamp = new Date().getTime();
 	
-    const onLeerTabla = (error, resultado) => {
+	persona.creado = timestamp;
+	persona.editado = timestamp;
+	
+	const parametros = {
+		TableName: process.env.TABLA_PERSONA,
+		Item: persona,
+	};
+	let resultado = await dynamoDb.put(parametros).promise();
+	return resultado;
+};
 
-        if (error) {
-            console.log('Fallo al leer los datos:', JSON.stringify(error, null, 2));
-            callback(error);
-        } else {
-            console.log("Lectura exitosa");
-			
-            return callback(null, {
-                statusCode: 200,
-                body: JSON.stringify({
-                    personas: resultado.Items
-                })
-            });
-        }
+function getRespuesta(resultado){
+	const respuesta = {
+        statusCode: 200,
+        body: JSON.stringify(resultado),
     };
-
-    dynamoDb.scan(parametros, onLeerTabla);
-}
+    return respuesta;
+};
